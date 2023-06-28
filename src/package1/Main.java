94abedb1b05247441c2a7bc906a9e352e46b2b7e
zhengzx <103814586+zzx0731@users.noreply.github.com>
2023-06-29 00:57:48 +0800

    你好

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -3,5 +3,6 @@ package package1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is my first Java code!");
+        System.out.println("你好");
     }
 }
