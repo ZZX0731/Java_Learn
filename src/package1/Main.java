08621080dae5b4d14cfbffe7befd88cd2adfa978
zhengzx <103814586+zzx0731@users.noreply.github.com>
2023-06-29 01:21:28 +0800

    设置主分支权限

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -2,6 +2,6 @@ package package1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("主分支不再允许直接推送。");
+        System.out.println("主分支不再允许直接推送");
     }
 }
