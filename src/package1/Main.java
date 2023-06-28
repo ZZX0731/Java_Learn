fdbe67931d859a8edc54d70dbc8eaa399eacd385
郑志贤 <2683713901@qq.com>
2023-06-29 00:19:57 +0800

    xx

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -3,6 +3,5 @@ package package1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is my first Java code!");
-        System.out.println("hello");
     }
 }
