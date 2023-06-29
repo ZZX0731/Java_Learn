5e88b2e6b3b8e20425e31cda1d9ad61dcccb699b
FJH <1574542635@qq.com>
2023-06-30 00:39:16 +0800

    随机生产1~100,猜出这个随机数

--- src/package1/randomNumber.java
+++ src/package1/randomNumber.java
@@ -1,7 +1,10 @@
 package package1;
+
 import com.sun.javaws.IconUtil;
 
 import java.util.Random;
+import java.util.Scanner;
+
 public class randomNumber {
     public static void main(String[] args) {
         Random r = new Random();
@@ -9,23 +12,25 @@ public class randomNumber {
         到(这个数-1)结束
         口诀:包头不包尾,包左不包右
          */
-            int number = r.nextInt(100)+1;
+        int number = r.nextInt(100) + 1;
             /*
             秘诀:用来生成任意数到任意数之间的随机数 1~100
             让这个范围的头尾都减去一个值,让这个范围从0开始 -1  0~99
             让尾+1  99+1=100
             最终的结果,再加上第一步减去的值
              */
-            System.out.println(number);
-            int tag=0;
-        for (int i = 0; i <= number; i++) {
-            if(i == number){
-             break;
+        System.out.println(number);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int guessNumber = sc.nextInt();
+            if (guessNumber > number) {
+                System.out.println("大了");
+            } else if (guessNumber == number) {
+                System.out.println("猜对了");
+                break;
+            } else {
+                System.out.println("小了");
+            }
         }
-            tag=i;
-        }
-        System.out.println(tag+1);
-
     }
 }
-
