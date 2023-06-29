879459f0e0df2625f16a8787dee00ebcbe3281b8
FJH <1574542635@qq.com>
2023-06-29 17:08:20 +0800

    编写找最多字符的函数

--- src/package1/FindChar.java
+++ src/package1/FindChar.java
@@ -3,36 +3,46 @@ package package1;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class FindChar {
     public static void main(String[] args) {
         System.out.println("这是找出现次数最多的字符的函数:");
+        Scanner in = new Scanner(System.in);
+        String string = in.next();
+        ArrayList<Character> list = findCharacter(string); // 得到的list就是出现次数最多的那几个字符
+        // 然后我输出这些字符,这个写法叫增强for循环
+        for(char c:list){
+            System.out.println(c+" ");
+        }
     }
 
-    // TODO: 把我给你的函数敲在这边
-    // TODO的意思是,在这里完成任务
-    public static ArrayList<Character> findCharacter(String string){
+    /**
+     *
+     * @param string 判断这个字符串中哪几个字符出现的最多
+     * @return 返回的是出现的最多的那几个字符
+     */
+    public static ArrayList<Character> findCharacter(String string) {
         int max = 0;
         ArrayList<Character> retval = new ArrayList<>();
-        // 你来
-        HashMap<Character,Integer> hashMap = new HashMap<Character,Integer>();
-        for(int i = 0;i <string.length();++i){
-            if(hashMap.containsKey(string.charAt(i))){
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < string.length(); ++i) {
+            if (hashMap.containsKey(string.charAt(i))) {
                 int value = hashMap.get(string.charAt(i));
                 value++;
-                hashMap.put(string.charAt(i),value);
-            }else{
-                hashMap.put(string.charAt(i),1);
+                hashMap.put(string.charAt(i), value);
+            } else {
+                hashMap.put(string.charAt(i), 1);
             }
         }
-        for(Map.Entry<Character,Integer> entry : hashMap.entrySet()) {
+        for (Map.Entry<Character, Integer> entry : hashMap.entrySet()) {
             max = max > entry.getValue() ? max : entry.getValue();
         }
-        for(Map.Entry<Character,Integer> entry : hashMap.entrySet()){
-            if(entry.getValue() == max){
+        for (Map.Entry<Character, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() == max) {
                 retval.add(entry.getKey());
             }
         }
-            return retval;
-        }
+        return retval;
+    }
 }
