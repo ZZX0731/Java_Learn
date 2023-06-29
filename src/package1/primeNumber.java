6bf9d6a06e0336501ba60bdfcc141a76b48a7595
zhengzx <103814586+zzx0731@users.noreply.github.com>
2023-06-29 20:30:57 +0800

    统一文件编码方式

--- src/package1/primeNumber.java
+++ src/package1/primeNumber.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class primeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int tag = 0;
-        for(int i = 2;i <=Math.sqrt(a);i++){
-            if(a % i == 0){
-                System.out.println("合数");
-                break;
-            }
-            tag = i;
-        }
-        if(tag == (int)Math.sqrt(a)){
+        int value = sc.nextInt();
+        if (IsPrime(value))
             System.out.println("质数");
+        else
+            System.out.println("合数");
+    }
+
+    // 判断质数
+    private static boolean IsPrime(int number) {
+        for (int i = 2; i < Math.sqrt(number); ++i) {
+            if (number % i == 0) {
+                return false;
+            }
         }
+        return true;
     }
 }
