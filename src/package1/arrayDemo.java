37890597322fdd4bdc2d75ce6cd7280e9e602bfc
FJH <1574542635@qq.com>
2023-06-30 16:52:23 +0800

    随机生产10个1~100的数存入数组,分别求出和,平均数以及有多少个数据比平均数小

--- src/package1/arrayDemo.java
+++ src/package1/arrayDemo.java
@@ -5,21 +5,22 @@ import java.util.Random;
 public class arrayDemo {
     public static void main(String[] args) {
         Random r = new Random();
-        int a[]= new int[10];
-        int sum1 =0;
+        int a[] = new int[10];
+        int sum1 = 0;
         for (int i = 0; i < a.length; i++) {
-            int number = r.nextInt(100)+1;
-            System.out.println("生成的随机数" + number);
+            int number = r.nextInt(100) + 1;
             a[i] = number;
-            sum1 = sum1 + number;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("生成的随机数" + a[i]);
+            sum1 = sum1 + a[i];
         }
         System.out.println("所有数据的和" + sum1);
-        int avg=0;
+        int avg = 0;
         avg = sum1 / 10;
         int sum2 = 0;
         for (int i = 0; i < a.length; i++) {
-            int number = r.nextInt(100)+1;
-            if(a[i] < avg){
+           if(a[i] < avg){
                 sum2++;
             }
         }
